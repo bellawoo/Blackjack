0b035e4050f3aa1a8f9ef49cdf86fa56b8a088f9
Bella <woo.bella@gmail.com>
2015-05-27 15:04:30 -0400

    initial commit. commented on rule of play

---
+++ play.rb
@@ -0,0 +1,37 @@
+require "./blackjack.rb"
+
+# set up human player
+
+# draw two cards for player
+
+# sum two cards
+
+# reveal two cards and sum to player
+
+# draw two cards for dealer
+
+# reveal one card of dealer's hand to player
+
+# if player cards = 21 --> win
+# else --> ask if player wants to hit or stand
+
+# HIT
+# draw another card for player
+# sum cards
+# if = 21 --> win
+# else --> ask if player wants to hit or stand
+# repeat
+
+# STAND
+# sum cards
+# if = 21 --> win
+# else --> return sum
+
+# Dealer hits
+# sum cards
+# if = 21 --> win
+# else hits
+# sum cards
+# repeat until > 17
+
+# compare player and dealer sums. larger sum wins.
